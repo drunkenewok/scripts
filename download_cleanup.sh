6eebb946b86f0429660aeb68739f195401884560
Brandon Horn <caladorn@ugcs.caltech.edu>
2013-02-05 15:11:29 -0700

    first commit

---
+++ download_cleanup.sh
@@ -0,0 +1,2 @@
+find ~/downloads -maxdepth 1 -type f -name *.torrent | xargs -t -i mv {} ~/downloads/torrents/watch/
+find ~/downloads -maxdepth 1 -type f -name *.jpg | xargs -t -i mv {} ~/pics/unsorted/
---
+++ tv_cleanup.sh
@@ -0,0 +1,2 @@
+find /home/bhorn/video/tv/The\ Colbert\ Report/ -mtime +21 | xargs -t -i rm -f {} ;
+find /home/bhorn/video/tv/The\ Daily\ Show\ with\ Jon\ Stewart/ -mtime +21 | xargs -t -i rm -f {} ;
